8b8e07c3a224903aaa035396bf29506247e17126
Andrea Paz <46249374+AndreaToPaz@users.noreply.github.com>
2023-01-27 22:41:28 -0400

    final

--- cc.py
+++ cc.py
@@ -8,19 +8,9 @@ data = []
 # text_list = list(np.concatenate(df_m[["comment"]].values.tolist()).flat)
 # print(text_list)
 
+df = pd.read_csv('doneT.csv', sep=';')
+print(df)
 
-df = pd.read_csv('done.csv', sep=';')
-data_t = df[["Tweet","Label"]].values.tolist()
+df2 = df[df.label != 'fear']
 
-# for i in range(len(text_list)):
-#             data.append([text_list[i],'surprise'])
-
-# for i in range(len(data_t)):
-#             data.append(data_t[i])
-            
-            
-df = pd.DataFrame(data_t, columns=["Tweet",'Label'])
-df.to_csv('doneFR.csv', sep =';', index=False, encoding="utf-8")
-
-
-
+print (df2)
